d623472744ee2909318ae1bb1da086d248c5afd4
wannacry3030 <wannacry.bat@proton.me>
2023-02-03 13:02:49 -0300

    att

--- LFI_scannerGUI.py
+++ LFI_scannerGUI.py
@@ -19,38 +19,40 @@ def run_scan():
         # checando o codigo de resposta do request
         if response.status_code == 200:
             # se a resposta for 200, o codigo é acessavel
-            result_label.config(text=f"[+] VULNERÁVEL: {test_url}")
+            result_label.config(text=result_label.cget("text") + f"[+] VULNERÁVEL: {test_url}\n")
         else:
             # se o status nao for 200, o codigo não é acessavel
             result_label.config(text=f"[-] Não VULNERÁVEL: {test_url}")
 
 # criando a janela GUI
 root = tk.Tk()
-root.geometry("650x200")
+root.geometry("900x400")
 root.title("LFI Scanner")
 root['bg'] = 'black'
 # criando o input
 url_label = tk.Label(root, text="URL:", bg='#000000', fg='#5bff45')
-url_label.grid(row=0, column=0, sticky="W")
+url_label.grid(row=0, column=0, sticky="W", padx=10,pady=10)
 
 # criando a entrada de texto
 entry = tk.Entry(root)
-entry.grid(row=0, column=1)
+entry.grid(row=1, column=1, padx=10,pady=10)
+
+#barra de progresso
+pb = ttk.Progressbar(root, orient='horizontal', mode='indeterminate', length=280)
+pb.grid(column=1,row=0,columnspan=2, padx=10,pady=10)
 
 # criando um botão pra começar o scan
 myFont = font.Font(family='Courier', weight='bold')
 scan_button = tk.Button(root, text="Scannear", command=run_scan, font=myFont, bg='#000000', fg='#5bff45', activebackground='#787878')
-scan_button.grid(row=1, column=0, columnspan=2, pady=10)
-
-#barra de progresso
-pb = ttk.Progressbar(root, orient='horizontal', mode='indeterminate', length=280)
+scan_button.grid(row=2, column=0, columnspan=2, padx=10,pady=10)
 
 # botao de resetar o scanner
 stop_button = tk.Button(root, text='Resetar',)
+stop_button.grid(row=3, column=0,  columnspan=2, padx=10,pady=10)
 
 # criando um rotulo que mostra o resultado
 result_label = tk.Label(root, text="",)
-result_label.grid(row=2, column=0, columnspan=2, pady=10)
+result_label.grid(row=4, column=0, columnspan=2, padx=10,pady=10)
 
 # startando a GUI
 root.mainloop()
