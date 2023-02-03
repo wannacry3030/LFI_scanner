1e666ddafe855bb9833413e46847607feddc97f8
wannacry3030 <wannacry.bat@proton.me>
2023-02-03 14:40:37 -0300

    primeiro design completo

--- LFI_scannerGUI.py
+++ LFI_scannerGUI.py
@@ -40,11 +40,6 @@ entry = tk.Entry(root)
 entry.pack(pady=10)
 #entry.grid(row=0, column=1, columnspan=2, padx=10, pady=10, sticky="E")
 
-#barra de progresso
-pb = ttk.Progressbar(root, orient='horizontal', mode='indeterminate', length=280)
-pb.pack(pady=10)
-#pb.grid(column=0,row=1,columnspan=2, padx=10,pady=10)
-
 # criando um botão pra começar o scan
 myFont = font.Font(family='Courier', weight='bold')
 scan_button = tk.Button(root, text="Scannear", command=run_scan, font=myFont, bg='#000000', fg='#5bff45', activebackground='#787878')
@@ -53,10 +48,17 @@ scan_button.pack(pady=10)
 
 # botao de resetar o scanner
 stop_button = tk.Button(root, text='Resetar', command=run_scan, font=myFont, bg='#000000', fg='#5bff45', activebackground='#787878')
+stop_button.pack(pady=10)
 #stop_button.grid(row=2, column=2, columnspan=2, padx=10, pady=10, sticky="E")
+
+#barra de progresso
+pb = ttk.Progressbar(root, orient='horizontal', mode='indeterminate', length=280)
+pb.pack(pady=10)
+#pb.grid(column=0,row=1,columnspan=2, padx=10,pady=10)
 
 # criando um rotulo que mostra o resultado
 result_label = tk.Label(root, text="",)
+result_label.pack(pady=10)
 #result_label.grid(row=4, column=0, columnspan=2, padx=10,pady=10)
 
 # startando a GUI
